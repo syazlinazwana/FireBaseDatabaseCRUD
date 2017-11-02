12f90768601dad19fa6619c88e8d81ac8137416a
SYAZLIN <nsyazlinazwana@gmail.com>
2017-11-02 11:20:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/syazlin/firebasedatabasecrud/Artist.java
@@ -0,0 +1,25 @@
+package com.example.syazlin.firebasedatabasecrud;
+
+/**
+ * Created by SYAZLIN on 2/11/2017.
+ */
+
+public class Artist {
+    String artistId;
+    String artistName;
+
+    public Artist(){}
+
+    public Artist(String artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+}
